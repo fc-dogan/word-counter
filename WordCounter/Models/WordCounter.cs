9010e927f161057526846acffeb146398ac202f1
Fatma <fceyda@gmail.com>
2020-03-06 12:04:39 -0800

    pass setSentence test

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -2,7 +2,7 @@ namespace WordCounter.Models
 {
   public class Counter 
   {
-    public string Sentence {get;}
+    public string Sentence {get; set;}
     public string Word {get; }
     public Counter(string word,string sentence)
     {
